3cacbaf66b55e2e387454388bb949f21f3adcf2d
Hiroshi <hiroshi.yamada426@gmail.com>
2018-05-04 18:16:29 +0900

    first AI mode

--- othello.js
+++ othello.js
@@ -1,4 +1,9 @@
+//初期の盤面
 var initialData = [[2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 0, 1, 2, 2, 2], [2, 2, 2, 1, 0, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 2]];
+//石の種別
+var WHITE = 0;
+var BLACK = 1;
+var EMPTY = 2;
 //盤面生成(送信ボタンが押される度に実行)
 var board = getBoard();
 createBoard(board);
@@ -73,30 +78,61 @@ function createBoard(data) {
 	div.innerHTML = board;
 }
 
-function calcOthello() {
+//人間の手を実行する
+function manOthello() {
 	var row = Number(document.form1.row.value);
 	var column = Number(document.form1.column.value);
 	var color = Number(document.form1.color.value);
 	if (row == 10 || column == 10 || color == 10) {
 		alert("行・列・石の色を全て選択してください。");
 	} else {
-		var flipCells = flipSearch(row, column, color);
 		//ひっくり返す石リストを取得
-		console.log(flipCells);
-		flip(flipCells, color);
+		var flipCells = flipSearch(row, column, color, "input");
 		//石をひっくり返す
+		flip(flipCells, color);
 	}
 }
 
-//ひっくり返すべき石を返す関数
-function flipSearch(row, column, color) {
-	var flipCells = [];
+//AI最善手を実行する
+function AIOthello(mode) {
+	//白色の石が置ける場所リストを取得
+	var position = getPossiblePosition(WHITE);
+	//手がない場合はalert表示して終了
+	if(position.length == 0){
+		alert("AIが指せる手がありません。");
+		return 0;
+	}
+	//AI最善手の行と列
+	var row = Number(position[0][0]);
+	var column = Number(position[0][1]);
+	//表示モードの場合は最善手を表示
+	if(mode == "view"){
+		//表示するAI最善手のテキスト
+		var txt = "AI最善手: " + "abcdefgh"[row] + "-" + "12345678"[column];
+		document.getElementById("AIStone").innerText = txt;
+	}
+	//実行モードの場合は石をひっくり返す
+	else if(mode == "execute"){
+		//ひっくり返す石リストを取得
+		var flipCells = flipSearch(row, column, WHITE, "input");
+		//石をひっくり返す
+		flip(flipCells, WHITE);
+	}
+}
+
+
+//ひっくり返すことのできる石を探索して返す
+function flipSearch(row, column, color, mode) {
 	//ひっくり返す石の位置を格納する
-	var board = getBoard();
+	var flipCells = [];
 	//盤面の状態
+	var board = getBoard();
 	//既に石が置いてある場合
-	if (board[row][column] != 2) {
-		alert("石が置ける場所を選択してください。");
+	if (board[row][column] != EMPTY) {
+		//実際に手を入力する場合はアラートを出す
+		if (mode == "input") {
+			alert("石が置ける場所を選択してください。");
+		}
 		return 0;
 	}
 	//上下左右斜めの8方向を順番に調べてひっくり返す
@@ -132,7 +168,9 @@ function flipSearch(row, column, color) {
 	}
 	//ひっくり返すべき石が見つからなかった場合
 	if (flipCells.length == 0) {
-		alert("石が置ける場所を選択してください。");
+		if (mode == "input") {
+			alert("石が置ける場所を選択してください。");
+		}
 		return 0;
 	}
 	return flipCells;
@@ -140,8 +178,29 @@ function flipSearch(row, column, color) {
 
 //石をひっくり返す関数
 function flip(cells, color) {
+	//石をひっくり返す処理
 	for (var i = 0; i < cells.length; i++) {
 		board[cells[i][0]][cells[i][1]] = color;
 	}
+	//盤面に設定
 	setBoard(board);
+}
+
+//石の置ける場所リストを取得
+function getPossiblePosition(color) {
+	//石が置ける場所リスト
+	var pStonePosition = [];
+	//現在の盤面を取得
+	var board = getBoard();
+	//全盤面に対して石が置ける場所を選ぶ
+	for (var row = 0; row < board.length; row++) {
+		for (var column = 0; column < board[0].length; column++) {
+			var cells = flipSearch(row, column, color, "search");
+			//もし石が置ける場合は場所リストに追加
+			if (cells.length > 0) {
+				pStonePosition.push([row, column]);
+			}
+		}
+	}
+	return pStonePosition;
 }
