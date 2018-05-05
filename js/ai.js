e141a5a57d8607c0080bef6be1e1e9b75501f5c4
Hiroshi <hiroshi.yamada426@gmail.com>
2018-05-05 13:06:35 +0900

    add curernt status mode

--- js/ai.js
+++ js/ai.js
@@ -69,3 +69,68 @@ function AIOthello(mode) {
 	}
 }
 
+//ひっくり返すことのできる石を探索して返す
+function flipSearch(row, column, color, mode) {
+	//ひっくり返す石の位置を格納する
+	var flipCells = [];
+	//盤面の状態
+	var board = getBoard();
+	//既に石が置いてある場合
+	if (board[row][column] != EMPTY) {
+		//実際に手を入力する場合はアラートを出す
+		if (mode == "input") {
+			alert("石が置ける場所を選択してください。");
+		}
+		return 0;
+	}
+	//上下左右斜めの8方向を順番に調べてひっくり返す
+	for (var drow = -1; drow <= 1; drow++) {
+		for (var dcolumn = -1; dcolumn <= 1; dcolumn++) {
+			//自分の現在地は何もしない
+			if (drow == 0 && dcolumn == 0) {
+				continue;
+			}
+			//壁の端まで上下左右斜めを延長して調べていく
+			for (var i = 1; i < initialData.length; i++) {
+				var nrow = row + i * drow;
+				var ncolumn = column + i * dcolumn;
+				//壁の端に到達してしまった場合は処理中止して次へ
+				if (nrow < 0 || nrow >= initialData.length || ncolumn < 0 || ncolumn >= initialData.length) {
+					break;
+				}
+				//移動した位置の石を取得
+				var cell = board[nrow][ncolumn];
+				//2つ以上延長した際の石がプレイヤーと同じ色の石だった場合
+				if (cell == color && i >= 2) {
+					//自分と間にある石をひっくり返すべき石として格納
+					for ( j = 0; j < i; j++) {
+						flipCells.push([row + j * drow, column + j * dcolumn]);
+					}
+				}
+				//プレイヤーと同じ石か何も置いていない部分に到達してしまった場合は処理中止して次へ
+				if (cell == color || cell == 2) {
+					break;
+				}
+			}
+		}
+	}
+	//ひっくり返すべき石が見つからなかった場合
+	if (flipCells.length == 0) {
+		if (mode == "input") {
+			alert("石が置ける場所を選択してください。");
+		}
+		return 0;
+	}
+	return flipCells;
+}
+
+//石をひっくり返す関数
+function flip(cells, color) {
+	//石をひっくり返す処理
+	for (var i = 0; i < cells.length; i++) {
+		board[cells[i][0]][cells[i][1]] = color;
+	}
+	//盤面に設定
+	setBoard(board);
+}
+
--- js/othello.js
+++ js/othello.js
@@ -4,9 +4,12 @@ var initialData = [[2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2,
 var WHITE = 0;
 var BLACK = 1;
 var EMPTY = 2;
-//盤面生成(送信ボタンが押される度に実行)
+//盤面生成と枚数表示(送信ボタンが押される度に実行)
 var board = getBoard();
 createBoard(board);
+window.onload = function(){
+	countStone();
+}
 
 //盤面状態をjsonをパースして取得する
 function getBoard() {
@@ -32,6 +35,7 @@ function resetBoard() {
 	localStorage.removeItem('board');
 	board = getBoard();
 	createBoard(board);
+	countStone();
 }
 
 //盤面の書き込み
@@ -93,67 +97,27 @@ function manOthello() {
 	}
 }
 
-//ひっくり返すことのできる石を探索して返す
-function flipSearch(row, column, color, mode) {
-	//ひっくり返す石の位置を格納する
-	var flipCells = [];
-	//盤面の状態
-	var board = getBoard();
-	//既に石が置いてある場合
-	if (board[row][column] != EMPTY) {
-		//実際に手を入力する場合はアラートを出す
-		if (mode == "input") {
-			alert("石が置ける場所を選択してください。");
-		}
-		return 0;
-	}
-	//上下左右斜めの8方向を順番に調べてひっくり返す
-	for (var drow = -1; drow <= 1; drow++) {
-		for (var dcolumn = -1; dcolumn <= 1; dcolumn++) {
-			//自分の現在地は何もしない
-			if (drow == 0 && dcolumn == 0) {
-				continue;
-			}
-			//壁の端まで上下左右斜めを延長して調べていく
-			for (var i = 1; i < initialData.length; i++) {
-				var nrow = row + i * drow;
-				var ncolumn = column + i * dcolumn;
-				//壁の端に到達してしまった場合は処理中止して次へ
-				if (nrow < 0 || nrow >= initialData.length || ncolumn < 0 || ncolumn >= initialData.length) {
-					break;
-				}
-				//移動した位置の石を取得
-				var cell = board[nrow][ncolumn];
-				//2つ以上延長した際の石がプレイヤーと同じ色の石だった場合
-				if (cell == color && i >= 2) {
-					//自分と間にある石をひっくり返すべき石として格納
-					for ( j = 0; j < i; j++) {
-						flipCells.push([row + j * drow, column + j * dcolumn]);
-					}
-				}
-				//プレイヤーと同じ石か何も置いていない部分に到達してしまった場合は処理中止して次へ
-				if (cell == color || cell == 2) {
-					break;
-				}
+//盤面上の枚数をカウントして表示
+function countStone(){
+	var borad = getBoard();
+	var cWhite = 0;
+	var cBlack = 0;
+	var cEmpty = 0;
+	for(var i = 0; i < board.length; i++){
+		for(var j = 0; j < board[0].length; j++){
+			if(board[i][j] == WHITE){
+				cWhite++;
+			}else if(board[i][j] == BLACK){
+				cBlack++;
+			}else if(board[i][j] == EMPTY){
+				cEmpty++;
 			}
 		}
 	}
-	//ひっくり返すべき石が見つからなかった場合
-	if (flipCells.length == 0) {
-		if (mode == "input") {
-			alert("石が置ける場所を選択してください。");
-		}
-		return 0;
+	var txt = "黒 : " + String(cBlack) + "枚 　白 : " + String(cWhite) + "枚";
+	//もし空のマスがなくなるか片方の石がなくなってゲーム終了なら
+	if(cEmpty == 0 || cWhite == 0 || cBlack == 0){
+		txt = "ゲーム終了です！最終結果......" + txt;
 	}
-	return flipCells;
-}
-
-//石をひっくり返す関数
-function flip(cells, color) {
-	//石をひっくり返す処理
-	for (var i = 0; i < cells.length; i++) {
-		board[cells[i][0]][cells[i][1]] = color;
-	}
-	//盤面に設定
-	setBoard(board);
+	document.getElementById("countStone").innerText = txt;
 }
